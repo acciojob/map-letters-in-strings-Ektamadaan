7f9fbf2d0b83b83bf9ed1368bad5f9d959e151f3
Ektamadaan <ektamadaan45@gmail.com>
2024-01-12 21:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function mapLetters(word) {
+    const letterMap = {};
+
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+        if (!letterMap[letter]) {
+            // If the letter is not a key in the object, initialize it with an array containing the current index.
+            letterMap[letter] = [i];
+        } else {
+            // If the letter is already a key, push the current index to its array of indexes.
+            letterMap[letter].push(i);
+        }
+    }
+
+    return letterMap;
+}
+
+const str1 = rompt("E")
+
+
+
+
+
